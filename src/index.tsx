3e114989d7821391c7fe2ecd03b3e5b64b26893f
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-06 19:25:49 +0200

    Add redux store to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { store } from './redux/store'
+import { Provider } from 'react-redux';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/styles.scss';
+import './styles/normalize.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
