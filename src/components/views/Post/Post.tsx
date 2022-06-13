eb18f9ae6256462818cbfba5b0bfbc243c430057
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-13 22:22:10 +0200

    Add post likiung functionality and tweak styles for post buttons

--- src/components/views/Post/Post.tsx
+++ src/components/views/Post/Post.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Post.module.scss';
+import { getPostById, likePost, dislikePost } from '../../../redux/slices/postsSlice';
+import { useAppDispatch, useAppSelector} from '../../../redux/hooks';
 
 type Props = {
   imageURL: string;
   username: string;
   pictureSrc: string;
   likes: number;
+  id: string;
 }
 
-const Post = React.forwardRef<HTMLDivElement, Props>(({imageURL, username, pictureSrc, likes}, ref) => {
+const Post = React.memo(React.forwardRef<HTMLDivElement, Props>(({imageURL, username, pictureSrc, likes, id}, ref) => {
 
+  const [liked, setLiked] = useState(false)
+  const dispatch = useAppDispatch();
+
+  const manageClick = (id: string) => {
+    setLiked(!liked);
+    changePostStatus(id)
+  }
+
+  const changePostStatus = (id: string) => {
+    if (!liked) dispatch(likePost(id));
+    if (liked) dispatch(dislikePost(id)); 
+  }
 
   return (
     <div ref={ref} className={styles.postContainer}>
@@ -20,14 +35,14 @@ const Post = React.forwardRef<HTMLDivElement, Props>(({imageURL, username, pictu
       <img src={imageURL} alt="post content" />
       <div className={styles.postLabel}>
         <div className={styles.postControls}>
-          <i className='fa fa-heart-o'></i>
-          <i className='fa fa-comment-o'></i>
-          <i className={`fa fa-location-arrow ${styles.arrow}`}></i>
+          <button onClick={() => manageClick(id)} className={styles.iconButton}><i className={`fa fa-heart ${styles.stroke}`}></i></button>
+          <button className={styles.iconButton}><i className={`fa fa-comment ${styles.stroke}`}></i></button>
+          <button className={styles.iconButton}><i className={`fa fa-location-arrow ${styles.stroke}`}></i></button>
         </div>
         <p className={styles.likes}>Total likes: {likes}</p>
       </div>
     </div>
   )
-})
+}))
 
 export default Post;
