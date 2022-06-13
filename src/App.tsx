d8d4e7d7ae05f4c99b2963681d65e1189e62b307
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-13 22:22:33 +0200

    implement react router in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Container from './components/common/Container/Container';
 import Navbar from './components/views/Navbar/Navbar';
-import PostsList from './components/features/PostsList/PostsList';
+import Home from './components/pages/Home/Home'
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Container>
-        <PostsList></PostsList>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </Container>
     </>
   );
