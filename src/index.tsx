d743048de0b8897e1b63d2e5320372e8a8720431
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-13 22:19:39 +0200

    Add browserrouter to access react router

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/styles.scss';
 import './styles/normalize.scss';
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
