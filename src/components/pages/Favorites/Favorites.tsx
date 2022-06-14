0f29212547f4309de836cf30433ecab3df6a3c6e
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-14 18:59:32 +0200

    Use favoritesList on favorites page (v1)

---
+++ src/components/pages/Favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import FavoritesList from "../../features/FavoritesList/FavoritesList";
+
+const Favorites = () => {
+  return (
+    <>
+      <h1>Favorites</h1>
+      <FavoritesList />
+    </>
+  )
+};
+
+export default Favorites;
