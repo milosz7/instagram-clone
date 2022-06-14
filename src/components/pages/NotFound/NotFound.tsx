4c3cb82d5d0718ae772a2aab539e235816a367d0
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-14 18:58:12 +0200

    Add NotFound component for incorrect URLs

---
+++ src/components/pages/NotFound/NotFound.tsx
@@ -0,0 +1,16 @@
+import styles from './NotFound.module.scss';
+import Button from '../../common/Button/Button';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className={styles.contentBox}>
+      <h1>The page you were looking for does not exist!</h1>
+      <Link to="/">
+        <Button>Return to homepage</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
