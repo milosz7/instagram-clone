69f01f27117d2c597aec5edb44f6324ca63884b7
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-14 13:54:41 +0200

    Remove button styles from post stylesheet and use IconButtons instead

--- src/components/views/Post/Post.tsx
+++ src/components/views/Post/Post.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import IconButton from '../../common/IconButton/IconButton';
 import styles from './Post.module.scss';
-import { getPostById, likePost, dislikePost } from '../../../redux/slices/postsSlice';
-import { useAppDispatch, useAppSelector} from '../../../redux/hooks';
+import { likePost, dislikePost } from '../../../redux/slices/postsSlice';
+import { useAppDispatch } from '../../../redux/hooks';
+import clsx from 'clsx';
+import IconButtonAnimated from '../../common/IconButtonAnimated/IconButtonAnimated';
 
 type Props = {
   imageURL: string;
@@ -9,40 +12,46 @@ type Props = {
   pictureSrc: string;
   likes: number;
   id: string;
-}
+};
 
-const Post = React.memo(React.forwardRef<HTMLDivElement, Props>(({imageURL, username, pictureSrc, likes, id}, ref) => {
+const Post = React.memo(
+  React.forwardRef<HTMLDivElement, Props>(({ imageURL, username, pictureSrc, likes, id }, ref) => {
+    const [liked, setLiked] = useState(false);
+    const dispatch = useAppDispatch();
 
-  const [liked, setLiked] = useState(false)
-  const dispatch = useAppDispatch();
+    const manageClick = (id: string) => {
+      setLiked(!liked);
+      changePostStatus(id);
+    };
 
-  const manageClick = (id: string) => {
-    setLiked(!liked);
-    changePostStatus(id)
-  }
+    const changePostStatus = (id: string) => {
+      if (!liked) dispatch(likePost(id));
+      if (liked) dispatch(dislikePost(id));
+    };
 
-  const changePostStatus = (id: string) => {
-    if (!liked) dispatch(likePost(id));
-    if (liked) dispatch(dislikePost(id)); 
-  }
-
-  return (
-    <div ref={ref} className={styles.postContainer}>
-      <div className={styles.profileData}>
-        <img className={styles.profilePicture} src={pictureSrc} alt="avatar" />
-        <p className={styles.profileNickname}>{username}</p>
-      </div>
-      <img src={imageURL} alt="post content" />
-      <div className={styles.postLabel}>
-        <div className={styles.postControls}>
-          <button onClick={() => manageClick(id)} className={styles.iconButton}><i className={`fa fa-heart ${styles.stroke}`}></i></button>
-          <button className={styles.iconButton}><i className={`fa fa-comment ${styles.stroke}`}></i></button>
-          <button className={styles.iconButton}><i className={`fa fa-location-arrow ${styles.stroke}`}></i></button>
+    return (
+      <div ref={ref} className={styles.postContainer}>
+        <div className={styles.profileData}>
+          <img className={styles.profilePicture} src={pictureSrc} alt="avatar" />
+          <p className={styles.profileNickname}>{username}</p>
         </div>
-        <p className={styles.likes}>Total likes: {likes}</p>
+        <img src={imageURL} alt="post content" />
+        <div className={styles.postLabel}>
+          <div className={styles.postControls}>
+            <IconButtonAnimated
+              icon="heart"
+              isClicked={liked}
+              colorClass="buttonLikedRed"
+              action={() => manageClick(id)}
+            />
+            <IconButton icon="comment" />
+            <IconButton icon="location-arrow" />
+          </div>
+          <p className={styles.likes}>Total likes: {likes}</p>
+        </div>
       </div>
-    </div>
-  )
-}))
+    );
+  })
+);
 
 export default Post;
