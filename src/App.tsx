74876404c539a3848b75e22e6d09f9a35434102d
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-14 19:00:09 +0200

    Add routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Container from './components/common/Container/Container';
 import Navbar from './components/views/Navbar/Navbar';
-import Home from './components/pages/Home/Home'
+import Home from './components/pages/Home/Home';
+import Discover from './components/pages/Discover/Discover';
+import Favorites from './components/pages/Favorites/Favorites';
 import { Route, Routes } from 'react-router';
+import Add from './components/pages/Add/Add';
+import Messages from './components/pages/Messages/Messages';
+import NotFound from './components/pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <Container>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='favorites' element={<Favorites />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path="add" element={<Add />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
     </>
