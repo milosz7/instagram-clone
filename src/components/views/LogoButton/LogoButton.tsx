6233c61c08b76d7f7356f3b19b7398bb3b0da0bb
Milosz B <miloszbargiel@gmail.com>
2022-06-26 20:30:49 +0200

    Add state reset functionality to the logo button

--- src/components/views/LogoButton/LogoButton.tsx
+++ src/components/views/LogoButton/LogoButton.tsx
@@ -1,11 +1,20 @@
 import styles from './LogoButton.module.scss';
 import { useMatch, useNavigate } from 'react-router';
 import React from 'react';
+import { persistor } from '../../..';
+import { useAppDispatch } from '../../../redux/hooks';
+import { clearPosts, fetchPostData } from '../../../redux/slices/postsSlice';
 
 const LogoButton = ({children}: {children: React.ReactNode}) => {
   const match = useMatch('/');
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const manageClick = () => {
+    if (match && window.scrollY === 0) {
+      persistor.purge();
+      dispatch(clearPosts());
+      dispatch(fetchPostData());
+    }else
     if (match) {
       window.scrollTo({
         top: 0,
