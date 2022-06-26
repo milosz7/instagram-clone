fb37d66fb6d495e58977aa67d38b95fcabb1ab44
Milosz B <miloszbargiel@gmail.com>
2022-06-26 15:45:47 +0200

    Add PostModal and its initial styles

--- src/components/views/PostModal/PostModal.tsx
+++ src/components/views/PostModal/PostModal.tsx
@@ -1,33 +1,37 @@
-import { useParams, useNavigate } from "react-router";
-import { Dialog } from "@reach/dialog";
-import "@reach/dialog/styles.css";
+import { useParams, useNavigate } from 'react-router';
+import { Dialog } from '@reach/dialog';
+import '@reach/dialog/styles.css';
 import styles from './PostModal.module.scss';
-import { useAppSelector } from "../../../redux/hooks";
-import { getPostById } from "../../../redux/slices/postsSlice";
+import { useAppSelector } from '../../../redux/hooks';
+import { getPostById } from '../../../redux/slices/postsSlice';
+import PostHeader from '../PostHeader/PostHeader';
+import PostControls from '../PostControls/PostControls';
+import PostInfo from '../PostInfo/PostInfo';
 
 const PostModal = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const postData = useAppSelector(state => getPostById(state, id))
-  
+  const postData = useAppSelector((state) => getPostById(state, id));
+
   const closeModal = () => {
-    navigate(-1)
-  }
-  console.log(postData)
+    navigate(-1);
+  };
   if (!postData) return null;
   return (
     <Dialog aria-labelledby="label" onDismiss={closeModal}>
       <div className={styles.modalContainer}>
-        <img className={styles.postImage} src={postData.imageURL} alt="post" />
-        <div>
-          <div>
-            <img className={styles.profilePicture} src={postData.picture} alt="avatar" />
-            <p>{postData.username}</p>
-          </div>
+        <div className={styles.imageContainer}>
+          <img className={styles.postImage} src={postData.imageURL} alt="post content" />
+        </div>
+        <div className={styles.postInfo}>
+          <PostHeader pictureSrc={postData.picture} username={postData.username} />
+          <div className={styles.commentBox}></div>
+          <PostInfo desc={postData.desc} likes={postData.likes} username={postData.username} />
+          <PostControls id={id} isFavorite={postData.isFavorite} />
         </div>
       </div>
     </Dialog>
-  )
-}
+  );
+};
 
 export default PostModal;
