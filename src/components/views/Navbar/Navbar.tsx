a284db59e8dde22e2c767dbef6f0b8e8750f4900
Milosz B <miloszbargiel@gmail.com>
2022-06-26 17:46:04 +0200

    Use searchbar in a navbar component

--- src/components/views/Navbar/Navbar.tsx
+++ src/components/views/Navbar/Navbar.tsx
@@ -2,32 +2,34 @@ import styles from './Navbar.module.scss';
 import { NavLink } from 'react-router-dom';
 import StrokeIcon from '../../common/StrokeIcon/StrokeIcon';
 import LogoButton from '../LogoButton/LogoButton';
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-        <div className={styles.navLayout}>
-          <LogoButton>Fakeinsta</LogoButton>
-          <div className={styles.iconBox}>
-            <NavLink to="/">
-              <StrokeIcon icon="home"/>
-            </NavLink>
-            <NavLink to="messages">
-              <StrokeIcon icon="location-arrow"/>
-            </NavLink>
-            <NavLink to="add">
-              <StrokeIcon icon="plus"/>
-            </NavLink>
-            <NavLink to="discover">
-              <StrokeIcon icon="compass"/>
-            </NavLink>
-            <NavLink to="favorites">
-              <StrokeIcon icon="heart"/>
-            </NavLink>
-          </div>
+      <div className={styles.navLayout}>
+        <LogoButton>Fakeinsta</LogoButton>
+        <SearchBar placeholder="Search" />
+        <div className={styles.iconBox}>
+          <NavLink to="/">
+            <StrokeIcon icon="home" />
+          </NavLink>
+          <NavLink to="messages">
+            <StrokeIcon icon="location-arrow" />
+          </NavLink>
+          <NavLink to="add">
+            <StrokeIcon icon="plus" />
+          </NavLink>
+          <NavLink to="discover">
+            <StrokeIcon icon="compass" />
+          </NavLink>
+          <NavLink to="favorites">
+            <StrokeIcon icon="heart" />
+          </NavLink>
         </div>
+      </div>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
