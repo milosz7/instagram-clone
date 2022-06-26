8ad4e06fd2ba8dbced4a4790dda78a4f964a7e63
Milosz B <miloszbargiel@gmail.com>
2022-06-26 20:27:29 +0200

    install redux persist and integrate it with the app

--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,23 @@ import { BrowserRouter } from 'react-router-dom'
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/styles.scss';
 import './styles/normalize.scss';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
+import Spinner from './components/common/Spinner/Spinner';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+export const persistor = persistStore(store)
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
         <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import postsReducer from './slices/postsSlice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({postsReducer})
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    postsReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
