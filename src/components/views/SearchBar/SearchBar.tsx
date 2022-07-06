c947b556f92d00f494972d638d3d94035e9dbb0d
Milosz B <miloszbargiel@gmail.com>
2022-07-06 23:46:23 +0200

    Add a logic to display SearchResults in the SearchBar component

--- src/components/views/SearchBar/SearchBar.tsx
+++ src/components/views/SearchBar/SearchBar.tsx
@@ -1,7 +1,26 @@
 import styles from './SearchBar.module.scss';
+import SearchResults from '../SearchResults/SearchResults';
+import { useState } from 'react';
 
-const SearchBar = ({ placeholder }: {placeholder: string}) => {
-  return <input className={styles.input} placeholder={placeholder}></input>
-}
+const SearchBar = ({ placeholder }: { placeholder: string }) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <div className={styles.layout}>
+      <input
+        type="text"
+        onBlur={() => setTimeout(() => {
+          setIsFocused(false)
+        }, 150)}
+        onFocus={() => setIsFocused(true)}
+        className={styles.input}
+        placeholder={placeholder}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      {isFocused && <SearchResults query={searchQuery} />}
+    </div>
+  );
+};
 
 export default SearchBar;
