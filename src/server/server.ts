d2a59496719444919ae986357bca2f5fa3f19ede
Milosz B <miloszbargiel@gmail.com>
2022-09-05 17:49:38 +0200

    Establish connection with db

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
+import { connectToDb } from '../utils/helpers';
 
 const port = process.env.PORT || 3001;
-const buildDir = path.join(process.cwd() + "/build");
+const buildDir = path.join(process.cwd() + '/build');
 
 const app = express();
+
+connectToDb();
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -13,8 +16,8 @@ app.use(
 );
 app.use(express.static(buildDir));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(buildDir, "index.html"));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(buildDir, 'index.html'));
 });
 
 app.listen(port);
