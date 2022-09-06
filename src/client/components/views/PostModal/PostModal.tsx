8bb02b32789bec332c87df26748a0b7c613d1cfe
Milosz B <miloszbargiel@gmail.com>
2022-09-06 15:37:59 +0200

    PostModal component refactor so it uses DB data

--- src/client/components/views/PostModal/PostModal.tsx
+++ src/client/components/views/PostModal/PostModal.tsx
@@ -1,22 +1,34 @@
 import { useParams, useNavigate } from 'react-router';
 import { Dialog } from '@reach/dialog';
 import '@reach/dialog/styles.css';
-import { useAppSelector } from '../../../redux/hooks';
-import { getPostById } from '../../../redux/slices/postsSlice';
 import PostExpanded from '../PostExpanded/PostExpanded';
+import { TrimmedPost } from '../../../../types/db-responses';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './PostModal.module.scss';
 
 const PostModal = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const postData = useAppSelector((state) => getPostById(state, id));
+  const [postData, setPostData] = useState<null | TrimmedPost>(null);
+  
+  useEffect(() => {
+    const fetchPostData = async () => {
+      const { data }: { data: TrimmedPost } = await axios.get(`/api/posts/${id}`);
+      setPostData(data);
+    };
+    fetchPostData();
+  });
 
   const closeModal = () => {
     navigate(-1);
   };
   if (!postData) return null;
   return (
-    <Dialog aria-labelledby="label" onDismiss={closeModal}>
+    <Dialog  aria-labelledby="label" onDismiss={closeModal}>
+      <div className={styles.dialog}>
       <PostExpanded {...postData} />
+      </div>
     </Dialog>
   );
 };
