c36fecf1b16657b2c050ffee60ac836306244a91
Milosz B <miloszbargiel@gmail.com>
2022-09-06 17:47:35 +0200

    Add publishedInfo generation to Post and PostExpanded components

--- src/client/components/views/PostExpanded/PostExpanded.tsx
+++ src/client/components/views/PostExpanded/PostExpanded.tsx
@@ -3,8 +3,23 @@ import PostControls from '../PostControls/PostControls';
 import PostInfo from '../PostInfo/PostInfo';
 import styles from './PostExpanded.module.scss';
 import { TrimmedPost } from '../../../../types/db-responses';
+import { msToPublishedInfo } from '../../../../utils/helpers-client';
+import { useMemo } from 'react';
 
-const PostExpanded = ({ id, desc, comments, published, likedBy, photo, avatar, username}: TrimmedPost) => {
+const PostExpanded = ({
+  id,
+  desc,
+  comments,
+  published,
+  likedBy,
+  photo,
+  avatar,
+  username,
+}: TrimmedPost) => {
+  const publishedInfo = useMemo(
+    () => msToPublishedInfo(Date.now() - Date.parse(published)),
+    [published]
+  );
   return (
     <div className={styles.modalContainer}>
       <div className={styles.imageContainer}>
@@ -14,7 +29,7 @@ const PostExpanded = ({ id, desc, comments, published, likedBy, photo, avatar, u
         <PostHeader pictureSrc={avatar} username={username} />
         <div className={styles.commentBox}></div>
         <PostControls id={id} />
-        <PostInfo desc={desc} likes={likedBy.length} username={username} />
+        <PostInfo desc={desc} likes={likedBy.length} username={username} published={publishedInfo} />
       </div>
     </div>
   );
