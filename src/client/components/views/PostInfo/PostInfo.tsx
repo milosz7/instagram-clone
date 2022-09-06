5c65e1a4d64df2fad90d8e692b84c8425c2370cb
Milosz B <miloszbargiel@gmail.com>
2022-09-06 16:01:13 +0200

    Make desc parameter optional in PostInfo component

--- src/client/components/views/PostInfo/PostInfo.tsx
+++ src/client/components/views/PostInfo/PostInfo.tsx
@@ -7,17 +7,19 @@ const PostInfo = ({
 }: {
   likes: number;
   username: string;
-  desc: string;
+  desc?: string;
 }) => {
   return (
     <div className={styles.container}>
       <p className={styles.bolded}>
         {likes} {likes !== 1 ? 'likes' : 'like'}
       </p>
-      <p className={styles.desc}>
-        <span className={styles.bolded}>{username + ' '}</span>
-        {desc}
-      </p>
+      {desc && (
+        <p className={styles.desc}>
+          <span className={styles.bolded}>{username + ' '}</span>
+          {desc}
+        </p>
+      )}
     </div>
   );
 };
