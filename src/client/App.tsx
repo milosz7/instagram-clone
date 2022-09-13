30469bfc04d9713f84b62154cc5a4523b4302ea6
Milosz B <miloszbargiel@gmail.com>
2022-09-13 14:02:07 +0200

    Add protection for routes before logging in

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,36 +4,61 @@ import Home from './components/pages/Home/Home';
 import Discover from './components/pages/Discover/Discover';
 import Favorites from './components/pages/Favorites/Favorites';
 import { Route, Routes, useLocation } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Add from './components/pages/Add/Add';
 import PostModal from './components/views/PostModal/PostModal';
 import Messages from './components/pages/Messages/Messages';
 import NotFound from './components/pages/NotFound/NotFound';
 import PostPage from './components/pages/PostPage/PostPage';
 import Profile from './components/pages/Profile/Profile';
+import Login from './components/pages/Login/Login';
 
 function App() {
-
   const location = useLocation();
-  let state = location.state as {backgroundLocation?: Location}
+  let state = location.state as { backgroundLocation?: Location };
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const isSessionActive = async () => {
+    const { data }: { data: { logged: boolean } } = await axios.get('/auth/is-logged', {
+      withCredentials: true,
+    });
+    setLoggedIn(data.logged);
+  };
+
+  useEffect(() => {
+    isSessionActive();
+  }, [loggedIn]);
+
+  if (!loggedIn)
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<Login action={setLoggedIn} />} />
+          <Route path="/register"></Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    );
 
   return (
     <>
       <Navbar></Navbar>
       <Container>
         <Routes location={state?.backgroundLocation || location}>
-          <Route path='/' element={<Home />} />
-          <Route path='profile/:username' element={<Profile />} />
-          <Route path='favorites' element={<Favorites />} />
-          <Route path='discover' element={<Discover />} />
-          <Route path='add' element={<Add />} />
-          <Route path='post/:id' element={<PostPage />}></Route>
-          <Route path='messages' element={<Messages />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="profile/:username" element={<Profile />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="add" element={<Add />} />
+          <Route path="post/:id" element={<PostPage />}></Route>
+          <Route path="messages" element={<Messages />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
         {state?.backgroundLocation && (
           <Routes>
-            <Route path='/post/:id' element={<PostModal />}></Route>
+            <Route path="/post/:id" element={<PostModal />}></Route>
           </Routes>
         )}
       </Container>
